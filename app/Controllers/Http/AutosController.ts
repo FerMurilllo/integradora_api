5fc61d24ce056a6c87599e733ae15c07de807d90
PedroDSM <pedro.koreano219@gmail.com>
2022-04-22 12:07:32 -0500

    se agrega la ultima ip de las bases de datos de mongodb

--- app/Controllers/Http/AutosController.ts
+++ app/Controllers/Http/AutosController.ts
@@ -5,9 +5,8 @@ import { connect } from 'mongoose';
 // const url = 'mongodb://3.140.240.243:27017/';
 
 const url = 'mongodb://127.0.0.1:27017,127.0.0.1:27018,127.0.0.1:27019/IntegradoraAPI';
-// const url = 'mongodb://127.0.0.1:27017,127.0.0.1:27018,127.0.0.1:27019/IntegradoraAPI';
-
-// const url = 'mongodb://3.140.240.243:27017,18.222.86.0:27017,/IntegradoraAPI'; 
+/*                                     /                                 //                             //                             */
+// const url = 'mongodb://3.140.240.243:27017,18.222.86.0:27017,3.138.67.71:27017/IntegradoraAPI'; 
 
 const auto = AutoModel.AutoModel; 
 
